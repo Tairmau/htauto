8c71a6a0d98776b934b9e5158a29feb567887779
Tairmau <133606363+Tairmau@users.noreply.github.com>
2023-08-25 02:36:26 -0400

    Add files via upload

---
+++ vues/v_avantlenvoie.php
@@ -0,0 +1,20 @@
+<div class="avantveille">
+    <p>Veuillez choisir vos disponibilités et nous joindre votre email afin de vous envoyer les informations pour la visite de votre futur véhicule</p>
+    <form action="index.php?uc=reservation&action=sendmail&id=<?php echo $id;?>" method="POST" >
+        <select name="date">
+            <option value="Lundi">Lundi</option>
+            <option value="Mardi">Mardi</option>
+            <option value="Mercredi">Mercredi</option>
+            <option value="Jeudi">Jeudi</option>
+            <option value="Vendredi">Vendredi</option>
+            <option value="Samedis">Samedis</option>
+        </select>
+        <select name="heure" id="">
+            <option value="9h-10h">9h-10h</option>
+            <option value="13h-14h">13h-14h</option>
+            <option value="15h-16h">15h-16h</option>
+        </select>
+        <input type="text" placeholder="Email" name="email">
+        <button type="submit">Envoyer</button>
+    </form>
+</div>
