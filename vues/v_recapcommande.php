21c5fda825992af610785322655444e456c757b8
Tairmau <133606363+Tairmau@users.noreply.github.com>
2023-08-25 02:33:17 -0400

    Add files via upload

---
+++ vues/v_recapcommande.php
@@ -0,0 +1,55 @@
+<section>
+    <div class="voitures">
+    <?php 
+            $pdo = PdoHtAuto::getPdoHtAuto();
+            $id = $_REQUEST['id'];
+            $req="select * from produits where id = '$id'";
+
+            $lesvoitures = $pdo->getLesProduits($req);
+
+            if(isset($_REQUEST['error'])){
+                $error_message = $_REQUEST['error'];
+                echo '<center><p class="error-message">' . $error_message . '</p></center>';
+            }
+
+            foreach($lesvoitures as $lavoiture){
+                $image = $lavoiture['image'];
+                $modele = $lavoiture['modele'];
+                $type = $lavoiture['type'];
+                $annee = $lavoiture['annee'];
+                $etat = $lavoiture['etat'];
+                 $prix =$lavoiture['prix'];
+        ?>
+        <div class="voiture">
+            <form action="">
+                <div class="left">
+                    <img src="../images/<?php echo $image ?>" alt="">
+                </div>
+                <div class="right">
+                    <div class="up">
+                        <h1><?php echo $modele ?></h1>
+                        <p class="type">Modèle : <?php echo $type ?></p>
+                        <div class="down">
+                            <p>Année : <?php echo $annee ?></p>
+                            <p>Etat : <?php echo $etat ?></p>
+                        </div>
+                        <h1><?php echo $prix ?> €</h1>
+                    </div>
+                    <?php
+                        if(isset($_SESSION['login'])){
+                            $id = $lavoiture['id'];
+                    ?>
+                        <button type="submit"><a href="index.php?uc=reservation&id=<?php echo $id;?>">RESERVER</a></button>
+                    <?php
+                        }
+                    ?>
+            </form>
+        </div>
+        <?php
+                break;
+
+            }
+        ?>
+    </div>
+    <div class="space"></div>
+</section>
---
+++ vues/v_tri.php
@@ -0,0 +1,6 @@
+<div class="tri">
+    <p>Trier par : </p>
+    <a href="index.php?uc=accueil&tri=prixcroissant">Prix croissant</a>
+    <a href="index.php?uc=accueil&tri=prixdecroissant">Prix décroissant</a>
+    <a href="index.php?uc=accueil&tri=annee">Récent</a>
+</div>
